15de3932e6688c26b6cc284f3ebd283c00b51995
Maksim Kryzhanovskii <krimaxim2010@yandex.ru>
2022-08-08 14:52:40 +0300

    book, bookDAO, book controller added. Show all book function added

---
+++ PetProjectLibrary/src/main/java/ru/kryz/max/controllers/BookController.java
@@ -0,0 +1,26 @@
+package ru.kryz.max.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.kryz.max.dao.BookDAO;
+
+@Controller
+@RequestMapping("/book")
+public class BookController {
+
+    private final BookDAO bookDAO;
+
+    @Autowired
+    public BookController(BookDAO bookDAO) {
+        this.bookDAO = bookDAO;
+    }
+
+    @GetMapping("/showAll")
+    public String showAll(Model model){
+        model.addAttribute("books", bookDAO.showAll());
+        return "books/showAll";
+    }
+}
